071c974c5ed723b765ea63188a7d68d4f530396a
ravitejashivanadhuni <ravitejashivanadhuni@gmail.com>
2026-04-01 17:28:02 +0530

    updated index.js

--- api/index.js
+++ api/index.js
@@ -2,17 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const serverless = require("serverless-http");
+
 const connectDB = require("../config/db");
 const jobRoutes = require("../routes/jobRoutes");
 const jobAlertRoutes = require("../routes/jobalertroutes");
 
-
-// require("./cron/jobstatusupdater"); //this is related to cron job status updater which will automatically update the status of jobs to "expired" when their expiry date has passed. It runs a scheduled task every day at midnight to check for expired jobs and update their status accordingly.
-
+// ❌ REMOVE cron
+// require("./cron/jobstatusupdater");
 
 const app = express();
 
-//cors
+// CORS
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
@@ -20,19 +20,18 @@ app.use(
     credentials: true
   })
 );
-// Middleware
+
 app.use(express.json());
 
-// Database connection
-// connectDB();
+// ✅ Connect DB once
+connectDB();
 
+// Routes
 app.use("/api", jobRoutes);
 app.use("/api/job-alerts", jobAlertRoutes);
-// Test Route
+
 app.get("/", (req, res) => {
   res.send("Daily Job Openings API Running");
 });
-
-const PORT = process.env.PORT || 5000;
 
 module.exports = serverless(app);
